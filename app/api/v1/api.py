1165489033f9ae6f8b5d826cff95c6ede9d588eb
Rifqi Setiawan <rifqis232@gmail.com>
2026-01-06 21:55:43 +0700

    fix: endpoint ibu hamil

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -2,7 +2,7 @@
 
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import auth, users, puskesmas, perawat
+from app.api.v1.endpoints import auth, users, puskesmas, perawat, ibu_hamil
 
 api_router = APIRouter(prefix="/api/v1")
 
@@ -10,5 +10,6 @@ api_router.include_router(auth.router)
 api_router.include_router(users.router)
 api_router.include_router(puskesmas.router)
 api_router.include_router(perawat.router)
+api_router.include_router(ibu_hamil.router)
 
 __all__ = ["api_router"]
